948463950e332c75103d0028f139686f632f817b
Mariana Alves <Mary_A2903@hotmail.com>
2023-07-13 19:06:56 -0300

    corrigindo erros

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const form = document.getElementById('form-formulario');
-var campoA = parseFloat(document.getElementById('valorA').value);
-var campoB = parseFloat(document.getElementById('valorB').value);
 let formEValido = false;
 
 function validaFormulario(campoA, campoB) {
@@ -10,15 +8,16 @@ function validaFormulario(campoA, campoB) {
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    var campoA = document.getElementById('valorA').value;
-    var campoB = document.getElementById('valorB').value;
-    const mensagemSucesso = `Está correto, o valor B: <b>${valorB}</b> é maior que o valor A: <b>${valorA}</b>`;
+    var campoA = parseFloat(document.getElementById('campoA').value);
+    var campoB = parseFloat(document.getElementById('campoB').value);
 
-    form = validarFormulario(campoA, campoB)
+    const mensagemSucesso = `Está correto, o valor B: <b>${campoB}</b> é maior que o valor A: <b>${campoA}</b>`;
+
+    formEValido = validaFormulario(campoA, campoB)
     if (formEValido) {
         const containerMensagemSucesso = document.querySelector('.success-message');
         containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.computedStyleMap.display = 'block';
+        containerMensagemSucesso.style.display = 'block';
 
         campoA.value = '';
         campoB.value = '';
@@ -28,15 +27,3 @@ form.addEventListener('submit', function(e){
     }
 })
 
-campoA.addEventListener('keyup', function(e) {
-    console.log(e.target.value);
-    formEValido = validaFormulario(e.target.value);
-
-    if (!formEValido) {
-        campoA.classList.add('error');
-        document.querySelector('.error-message').style.display = 'block';
-    } else {
-        campoA.classList.remove('error');
-        document.querySelector('.error-message').style.display = 'none';
-    }
-})
